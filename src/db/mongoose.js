bd36326ae499b81ebfa361ec1747d377736778cd
saim <ahmad.saim9@gmail.com>
2021-08-29 21:09:07 +0530

    environment name fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const CONNECTION_URI = process.env.MONGOD_URI || "mongodb://127.0.0.1:27017/todo-list-api"
+const CONNECTION_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/todo-list-api"
 mongoose.connect(CONNECTION_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
